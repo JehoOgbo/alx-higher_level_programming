c4802521cdf8501a90069cf0385a63b9027835ac
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-11-29 19:37:11 +0000

    Fixed errors of simply undesired syntax

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # print ascii alphabets in lowercase not followed by newline without q and e
 for i in range(97, 123):
-    if (i != 101) and (i != 113):
+    if i != 101 and i != 113:
         print("{}".format(chr(i)), end='')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # prints a string in uppercase followed by a new line
 def uppercase(str):
-    new = str
-    for i in new:		# iterate through all the chars in the word
-        num = ord(i)	# get the ascii equivalent of each character
-        char = i		# store the char in a new variable
-        if (num >= 97) and (num <= 122):  # if lower, assign uppercase value
-            char = chr(num - 32)
-        print("{:s}".format(char), end='')	# print the char
-    print("")				# print terminating newline character
+    """Print a string in uppercase."""
+    for i in str:  # iterate through all the chars in the word
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
