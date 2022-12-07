5514c525f1ca42317d25298c4dbfc4170310ed7c
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-12-07 14:29:59 +0000

    Made more progress
    Written python function to convert from roman Numerals to Hindu-Arabic Numerals.
    This is contained in the file 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,5 +9,22 @@ def roman_to_int(roman_string):
     """
     if roman_string is None:
         return 0
-    if roman_string is not str:
+    if type(roman_string) is not str:
         return 0
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100}
+    num['D'] = 500
+    num['M'] = 1000
+    last = 0
+    number = 0
+    for i in reversed(roman_string):
+        if i in num:
+            if num[i] >= last:
+                last = num[i]
+                number += last
+            else:
+                last = num[i]
+                number -= last
+        else:
+            print("Not a roman Numeral")
+            return 0
+    return number
