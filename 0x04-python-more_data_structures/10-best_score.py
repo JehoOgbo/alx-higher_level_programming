080006e69a05d67b918a62f5404025caff85c0fd
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-12-07 14:48:36 +0000

    Make function work if empty dictionary is entered

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,6 +10,8 @@ def best_score(a_dictionary):
     '''
     if a_dictionary is None:
         return (None)
+    if len(a_dictionary) == 0:
+        return (None)
     old = 0
     for k, v in a_dictionary.items():
         if v > old:
