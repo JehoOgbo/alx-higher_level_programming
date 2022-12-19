c7ff2467522cf1d400907922ccec1a34f6aed6e5
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-12-19 17:42:57 +0000

    Do advanced tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    from sys import stderr
+    """
+    prints an integer stored in value
+
+    value: may contain an integer
+
+    Return: true if value has been printed false otherwixe
+    then print error on stderr
+    """
+    try:
+        print("{:d}".format(value))
+    except ValueError as ex:
+        stderr.write("Exception: {}\n".format(ex.args[0]))
+        return False
+    else:
+        return True
