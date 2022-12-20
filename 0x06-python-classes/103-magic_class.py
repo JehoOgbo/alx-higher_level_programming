988bf20bafe84bfa089d0f5de1c84a40ee643498
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2022-12-20 20:31:38 +0000

    Advanced tasks

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Define a Magic class corresponding to some python bytecode"""
+
+import math
+
+
+class MagicClass:
+    """Represents a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+
+        Arg:
+            radius (float or int): The radius of the circle.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return the area of the circle."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Return the circumference of the circle."""
+        return (2 * math.pi * self.__radius)
