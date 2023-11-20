c1287428d4a8e49187d3c6e55fa69682e82b95d6
JehoOgbo <uromjehoshaphat@gmail.com>
2023-11-20 10:30:40 -0800

    fixing errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            print("{}".format(my_list[i]))
+            print("{}".format(my_list[i]), end='')
         except IndexError:
             print("Index does not exist")
         i += 1
