5d4a466dcf1092d6c3e3d035f70ee24715a17b99
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2023-12-07 17:16:43 +0000

    Doing more tasks

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Appends a string at the end of a text file """
+
+
+def append_write(filename="", text=""):
+    """ append_write: appends string at end of text file
+        Args":
+
+		filename - file to which string is added
+		text - text to be added
+        Return: No of chars appended
+    """
+    with open(filename, 'a', encoding='utf-8') as openfile:
+        # using with ensures the file will close
+        chars = openfile.write(text)
+    return chars
