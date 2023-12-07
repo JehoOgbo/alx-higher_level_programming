18e6b74c4e2c440f18462add0394bb377a0e51bd
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2023-12-07 17:18:12 +0000

    conform with pycodestyle

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,8 +7,8 @@ def append_write(filename="", text=""):
     """ append_write: appends string at end of text file
         Args":
 
-		filename - file to which string is added
-		text - text to be added
+        filename - file to which string is added
+        text - text to be added
         Return: No of chars appended
     """
     with open(filename, 'a', encoding='utf-8') as openfile:
