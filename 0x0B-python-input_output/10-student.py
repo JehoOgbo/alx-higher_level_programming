7ce314091b2e7cd1110e634d5b63ff6aba0d9e9d
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2023-12-08 08:59:33 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" class that defines a student """
+
+
+class Student:
+    """ This is a class that defines a student by their
+        firstname, lastname and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize the class with info about the student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a student"""
+        if type(attrs) is not list:
+            return self.__dict__
+        for item in attrs:
+            if type(item) is not str:
+                return self.__dict__
+        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
