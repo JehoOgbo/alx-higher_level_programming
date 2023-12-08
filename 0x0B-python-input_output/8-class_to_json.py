b6f72ec0b46aa4fb5c1a33c15b78cb3e5afb7322
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2023-12-08 08:23:24 +0000

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" returns the dictionary desc with simple data structure
+    for JSON serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    class_to_json: returns dict desc with simple data structure
+    for JSON serialization of an object
+    Args:
+
+    obj: instance of a class
+    note: all attributes of a class are serializable
+    """
+    return (obj.__dict__)
