2f409232d582710fde12c1b1e70388a9c16a743e
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-01-17 22:35:08 +0100

    update file

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 --Creates a new database with a given name
 --Don't fail if the database already exists
 --Don't use the select or show statements
-CREATE DATABASE hbtn_0c_0
+CREATE DATABASE if NOT EXISTS hbtn_0c_0;
