7de814408c89a71ff7af2b75fedebef7e1432e32
Jehoshaphat Urom <jehoshaphaturom@gmail.com>
2024-06-12 07:32:41 -0700

    done finally

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
---Creates a new database with a given name
---Don't fail if the database already exists
---Don't use the select or show statements
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- Creates a new database with a given name
+-- Dont fail if the database already exists
+-- Dont use the select or show statements
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
