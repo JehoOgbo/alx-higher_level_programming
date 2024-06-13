3b4028fcd8b246cc67c92202e81ba90a937d1507
Jehoshaphat Urom <jehoshaphaturom@gmail.com>
2024-06-13 14:36:13 -0700

    read well

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Create a database and a user
+-- script to run msql commands
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2' WITH PASSWORD AS 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
