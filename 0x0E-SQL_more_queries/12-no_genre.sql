833879cc46e20897cdcb3e099a72d0e9b57d1706
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-14 17:16:02 +0100

    left join

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- list all shows in which genre is NULL
 SELECT show.title, genre.genre_id
   FROM tv_shows AS show
-  RIGHT JOIN tv_show_genres AS genre
+  LEFT JOIN tv_show_genres AS genre
   ON show.id == genre.show_id
   WHERE genre.genre_id IS NULL
   ORDER BY s.title, g.genre.id;
