9e1f666aec111cd0c05dd44d9746c0a84d589015
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-14 17:22:59 +0100

    make it work

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,8 @@
--- list all shows in which genre is NULL
-SELECT show.`title`, genre.`genre_id`
-  FROM tv_shows AS show
-  LEFT JOIN tv_show_genres AS genre
-  ON show.id = genre.show_id
-  WHERE genre.genre_id IS NULL
-  ORDER BY s.title, g.genre.id;
+-- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+LEFT JOIN `tv_show_genres` AS g
+ON s.`id` = g.`show_id`
+WHERE g.`genre_id` IS NULL
+ORDER BY s.`title`, g.`genre_id`;
