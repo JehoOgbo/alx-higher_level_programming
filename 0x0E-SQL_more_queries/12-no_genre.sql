0fd3415c06ed41d2e245fd7a5a1761ae1627fe32
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-14 17:18:16 +0100

    genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 SELECT show.title, genre.genre_id
   FROM tv_shows AS show
   LEFT JOIN tv_show_genres AS genre
-  ON show.id == genre.show_id
+  ON show.id = genre.show_id
   WHERE genre.genre_id IS NULL
   ORDER BY s.title, g.genre.id;
