3c1dfe5969d04e0207ba07de3fc7e5efdaf60c28
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-16 14:45:22 +0100

    fix semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* prints the value of the argument passed to it */
-if (typeof process.argv[2] !== "undefined") {
-	console.log(process.argv[2]);
+if (typeof process.argv[2] !== 'undefined') {
+  console.log(process.argv[2]);
 } else {
-	console.log("No argument");
+  console.log('No argument');
 }
