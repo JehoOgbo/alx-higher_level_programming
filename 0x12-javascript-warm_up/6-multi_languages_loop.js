1ab8282e61b44103627c3eaf5c43af0275326b57
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-16 15:49:26 +0100

    Use a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* prints 3 lines by using an array of string and a loop */
+const names = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(names[i]);
+  i++;
+}
