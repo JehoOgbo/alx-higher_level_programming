e75f2870d8b78d68439b62fdd90f55075e2fb6cc
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-16 16:30:35 +0100

    fixed code doc

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,17 +2,16 @@
 /* prints a square */
 const x = process.argv[2];
 if (isNaN(x)) {
-	console.log("Missing size");
+  console.log('Missing size');
 }
 let row = 0;
 let column = 1;
 let exes = 'X';
 while (column < x) {
-	exes = exes + 'X';
-	column++;
+  exes = exes + 'X';
+  column++;
 }
-while (row < x)
-{
-	console.log(exes);
-	row++;
+while (row < x) {
+  console.log(exes);
+  row++;
 }
