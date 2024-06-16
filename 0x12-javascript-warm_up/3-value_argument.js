8e4aeeb5d26558e820de94f6218f54ec038ad431
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-16 14:42:25 +0100

    remove length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 /* prints the value of the argument passed to it */
-const count = process.argv.length;
 if (typeof process.argv[2] !== "undefined") {
 	console.log(process.argv[2]);
 } else {
