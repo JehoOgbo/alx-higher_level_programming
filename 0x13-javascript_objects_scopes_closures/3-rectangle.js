3e824973288614275537f50e6f9671ce24f2af2d
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-17 01:06:43 +0100

    build rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let i = 1;
+	let exes = 'X';
+	while (i < this.width) {
+      exes = exes + 'X';
+	  i++;
+    }
+	i = 0;
+    while (i < this.height) {
+      console.log(exes);
+      i++;
+    }
+  }
+}
+
+module.exports = Rectangle;
