5dd54e092ad0a0e68ac9670c23f70da2960bf8c6
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-18 13:29:07 +0100

    status

---
+++ 0x14-javascript-web_scraping/2-status.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* displays the status of a GET request */
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error === null) {
+    console.log('code:', response.statusCode);
+  }
+  // console.log('body:', body);
+}
+);
