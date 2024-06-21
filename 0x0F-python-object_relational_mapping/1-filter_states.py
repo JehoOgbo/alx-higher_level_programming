107b0f2eeaf6288033580e432b62a3433485e8cf
Urom Jehoshaphat Ogbonnia <uromjehoshaphat@gmail.com>
2024-06-21 22:41:23 +0100

    fix doc style issues

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" Lists all states with a name starting with N """
 import MySQLdb
 import sys
-""" Lists all states with a name starting with N """
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",user=sys.argv[1],
                          passwd=sys.argv[2],db="hbtn_0e_0_usa",port=3306)
