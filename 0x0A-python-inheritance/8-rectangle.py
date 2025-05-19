2050ac4d8a9d3f1fe2c6d7d573ffda52332e1484
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-05-19 21:51:36 +0100

    Fix all errors

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """A class that inherits some methods. Parentclass: BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
