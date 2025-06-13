8de8c19bd8a9eceb603e74d78c7df5bf696cff5c
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-13 20:12:58 +0100

    make model using orm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" class model for a mysql database table """
+""" defines a state class """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
+
 mymetaData = MetaData()
-Base = declarative_base(metadata=mymetaData)
+Base = declarative_base()
+
 
 class State(Base):
     """
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
