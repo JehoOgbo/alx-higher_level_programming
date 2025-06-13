5e781f22d7494179d130c588cd5970645e31171b
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-13 14:22:56 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" display all entries where name is passed as arg """
+""" lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
-if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    query1 = ("SELECT * FROM states WHERE name LIKE"
-              "'{}' ORDER BY id ASC").format(sys.argv[4])
-    cur.execute(query1)
-    rows = cur.fetchall()
-    for row in rows:
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states WHERE\
+                name='{}' ORDER BY id ASC".format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
-    db.close()
+    conn.close()
