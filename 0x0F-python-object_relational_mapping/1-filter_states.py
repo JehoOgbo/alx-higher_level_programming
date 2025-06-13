e2f417af0906535a1a302ebb8edb85cacbf21496
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-13 14:03:16 +0100

    make case insensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM states WHERE\
-                name LIKE 'N%' ORDER BY id ASC")
+                UPPER(name) LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
