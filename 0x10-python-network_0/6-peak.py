cbe27c6b48243ce81517c9d35c5dc7fb33df19d1
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-27 19:51:35 +0100

    change back

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,14 +6,8 @@ def find_peak(list_of_integers):
     """ finds the largest integer in the list """
     if list_of_integers is None or list_of_integers == []:
         return None
-    i = 0
     save = 0
-    try:
-        while list_of_integers[i]:
-            if save < list_of_integers[i]:
-                save = list_of_integers[i]
-            if save < list_of_integers[i + 1]:
-                save = list_of_integers[i + 1]
-            i += 2
-    except IndexError:
-        return save
+    for item in list_of_integers:
+        if save < item:
+            save = item
+    return save
