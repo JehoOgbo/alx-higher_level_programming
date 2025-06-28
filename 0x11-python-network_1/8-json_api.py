e33cdfd6e2c53e5f9e5f2b3715d002894fcdc826
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-28 21:49:45 +0100

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and
+sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+from sys import argv
+from requests import post
+
+
+if __name__ == '__main__':
+    try:
+        q = argv[1]
+    except IndexError:
+        q = ''
+    if len(q) > 1:
+        q = ''
+    response = post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r = response.json()
+        if r == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
