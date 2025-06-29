99afb7af9a8be2ecc768ad729515314fa12f1296
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-29 16:25:38 +0100

    fix error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,6 +3,7 @@
 Python script that takes in a letter and
 sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter
+update
 """
 from sys import argv
 from requests import post
