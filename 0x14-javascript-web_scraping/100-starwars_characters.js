e148578fbf2773fc8e727469557bf47b23162197
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-08-02 18:33:33 +0100

    advanced task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
-
+/* Prints all characters of a star wars movie */
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-let def = {};
+const movieId = process.argv[2];
+let url = 'https://swapi-api.alx-tools.com/api/films/';
+url = url + movieId;
+
 request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
-  } else {
-    const abc = JSON.parse(body);
-    abc.characters.forEach(function (item, index, array) {
-      request(item, function (error, response, content) {
-        if (error) {
-          console.log(error);
-        } else {
-          def = JSON.parse(content);
-          console.log(def.name);
-        }
-      });
+    return;
+  }
+  const movieDetails = JSON.parse(body).characters;
+  for (let i = 0; i < movieDetails.length; i++) {
+    request(movieDetails[i], function (error, response, body) {
+      if (error) {
+        return;
+      }
+      const charDetails = JSON.parse(body).name;
+      console.log(charDetails);
     });
   }
 });
